eaeee382b102db45c052e90b35b7379d467f9c97
AleksandarBorisov <a_borisov@abv.bg>
2018-10-04 15:01:11 +0300

    Initialize Project Skeleton

---
+++ AlphaCinemaData/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlphaCinemaData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
