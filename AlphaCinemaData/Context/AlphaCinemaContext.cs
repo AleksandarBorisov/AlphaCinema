4e051adf2641f2212b8646c12b904732ccc49e17
AleksandarBorisov <a_borisov@abv.bg>
2018-10-04 15:54:35 +0300

    Started Context Definition

---
+++ AlphaCinemaData/Context/AlphaCinemaContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlphaCinemaData.Context
+{
+    public class AlphaCinemaContext : DbContext
+    {
+        //public DbSet<Cities> Cities { get; set; }
+        //public DbSet<Genres> Genres { get; set; }
+        //public DbSet<MovieGenres> MovieGenres { get; set; }
+        //public DbSet<MovieProjections> MovieProjections { get; set; }
+        //public DbSet<Movies> Movies { get; set; }
+        //public DbSet<Projections> Projections { get; set; }
+        //public DbSet<Users> Users { get; set; }
+        //public DbSet<VisitedProjections> WatchedMovies { get; set; }
+
+
+        //protected override void OnModelCreating(ModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<Movies>()
+        //        .HasKey(movie => movie.ID);
+
+        //    base.OnModelCreating(modelBuilder);
+        //}
+    }
+}
